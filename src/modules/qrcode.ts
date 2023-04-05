5b88ef9dd9de2547046412e14708febdd7bf0b9f
bianyu <by15242952083@outlook.com>
2023-04-05 19:02:05 +0800

    clear master

--- src/modules/qrcode.ts
+++
@@ -1,5 +0,0 @@
-import VueQrcode from '@chenfengyuan/vue-qrcode'
-import type { UserModule } from '~/types'
-export const install: UserModule = ({ app }) => {
-  app.component('VueQrcode', VueQrcode)
-}
--- src/modules/wxLogin.ts
+++
@@ -1,5 +0,0 @@
-import wxlogin from 'vue-next-wxlogin'
-import { type UserModule } from '~/types'
-export const install: UserModule = ({ app }) => {
-  app.component('WxLogin', wxlogin)
-}
